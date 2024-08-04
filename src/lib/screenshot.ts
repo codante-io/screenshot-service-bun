e62098a599edeb4a2d0508119f55f248e95ccd44
Roberto Cestari <robertotcestari@gmail.com>
2024-08-04 14:10:06 -0300

    .env

--- src/lib/screenshot.ts
+++ src/lib/screenshot.ts
@@ -7,18 +7,13 @@ export async function getBufferFromPageScreenshot(
 ) {
   // Added timeout parameter with default value
   try {
-    console.log('screenshot');
     const browser = await firefox.launch();
-    console.log('screenshot2');
     const page = await browser.newPage();
     await page.setViewportSize({ width: 1920, height: 1080 });
 
     await page.goto(url, { timeout, waitUntil: 'domcontentloaded' }); // Added timeout option to page.goto()
-    console.log('screenshot3');
     let screenshotBuffer = await page.screenshot({ timeout }); // Added timeout option to page.screenshot()
-    console.log('screenshot4');
     await browser.close();
-    console.log('screenshot5');
 
     const image = sharp(screenshotBuffer);
     await image.metadata();
