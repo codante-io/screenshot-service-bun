1911be8003ee7671b3d79dd0fb0a51144406f3dd
Roberto Cestari <robertotcestari@gmail.com>
2024-08-04 17:05:42 -0300

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,12 @@
 import { Hono } from 'hono';
-import { getBufferFromPageScreenshot } from './lib/screenshot';
-import { S3 } from './lib/s3';
-import {
-  screenshotPutValidator,
-  screenshotValidator,
-} from './lib/hono-validators';
 import { bearerAuth } from 'hono/bearer-auth';
+import screenshot from './routes/screenshot';
 
 const app = new Hono();
+
 app.use('*', bearerAuth({ token: process.env.TOKEN! }));
 
-app.post('/screenshot', screenshotValidator(), async (c) => {
-  const { url, fileName } = c.req.valid('json');
-
-  try {
-    const screenshotBuffer = await getBufferFromPageScreenshot(url);
-    const s3 = new S3();
-    const { imageUrl } = await s3.uploadImage(fileName, screenshotBuffer);
-
-    return c.json({ message: 'Screenshot uploaded to S3', imageUrl });
-  } catch (e: any) {
-    return c.json({ message: e.message }, 500);
-  }
-});
-
-app.put('/screenshot', screenshotPutValidator(), async (c) => {
-  const { url, fileName, oldFilename } = c.req.valid('json');
-
-  try {
-    const screenshotBuffer = await getBufferFromPageScreenshot(url);
-    const s3 = new S3();
-    const { imageUrl } = await s3.uploadImage(fileName, screenshotBuffer);
-
-    // delete the old image
-    await s3.deleteImage(oldFilename);
-
-    return c.json({ message: 'Screenshot updated on S3', imageUrl });
-  } catch (e: any) {
-    return c.json({ message: e.message }, 500);
-  }
-});
+app.route('/screenshot', screenshot);
 
 export default {
   port: 3012,
---
+++ src/routes/screenshot.ts
@@ -0,0 +1,42 @@
+import { Hono } from 'hono';
+import {
+  screenshotPutValidator,
+  screenshotValidator,
+} from '../lib/hono-validators';
+import { getBufferFromPageScreenshot } from '../lib/screenshot';
+import { S3 } from '../lib/s3';
+
+const app = new Hono();
+
+app.post('/', screenshotValidator(), async (c) => {
+  const { url, fileName } = c.req.valid('json');
+
+  try {
+    const screenshotBuffer = await getBufferFromPageScreenshot(url);
+    const s3 = new S3();
+    const { imageUrl } = await s3.uploadImage(fileName, screenshotBuffer);
+
+    return c.json({ message: 'Screenshot uploaded to S3', imageUrl });
+  } catch (e: any) {
+    return c.json({ message: e.message }, 500);
+  }
+});
+
+app.put('/', screenshotPutValidator(), async (c) => {
+  const { url, fileName, oldFilename } = c.req.valid('json');
+
+  try {
+    const screenshotBuffer = await getBufferFromPageScreenshot(url);
+    const s3 = new S3();
+    const { imageUrl } = await s3.uploadImage(fileName, screenshotBuffer);
+
+    // delete the old image
+    await s3.deleteImage(oldFilename);
+
+    return c.json({ message: 'Screenshot updated on S3', imageUrl });
+  } catch (e: any) {
+    return c.json({ message: e.message }, 500);
+  }
+});
+
+export default app;
