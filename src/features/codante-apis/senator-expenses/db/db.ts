ca9da5578a670cd34cdc035f99b900f0013b0742
Roberto Cestari <robertotcestari@gmail.com>
2024-09-16 08:49:02 -0300

    refactor: simplify database connection setup and remove global variable usage

--- src/features/codante-apis/senator-expenses/db/db.ts
+++ src/features/codante-apis/senator-expenses/db/db.ts
@@ -1,9 +1,6 @@
 import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
-
-declare global {
-  var _db: ReturnType<typeof drizzle> | undefined;
-}
+import * as schema from './schema';
 
 const poolConnection = mysql.createPool({
   user: process.env.DATABASE_USER_SENATOR_EXPENSES,
@@ -11,10 +8,6 @@ const poolConnection = mysql.createPool({
   database: process.env.DATABASE_SENATOR_EXPENSES,
 });
 
-const db = globalThis._db || drizzle(poolConnection);
-
-if (process.env.NODE_ENV !== 'production') {
-  globalThis._db = db;
-}
+const db = drizzle(poolConnection, { schema, mode: 'default' });
 
 export { db, poolConnection };
